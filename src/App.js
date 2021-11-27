f2b4bd92706781cff119d08bf9896b231dbbee5e
Jack Dowdeswell <jack.dowdeswell@outlook.com>
2021-11-27 14:43:58 +0000

    headline component created and tested

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
+import Headlines from './Headlines';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/digital-futures-academy"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Digital Futures Academy
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://github.com/digital-futures-academy"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Digital Futures Academy
+          </a>
+        </header>
+      </div>
+
+      <Headlines />
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Headlines from './Headlines';
 
 test('renders DFA Github link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Digital Futures Academy/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders a headline through headline component', () => {
+  render(<Headlines />);
+  const testHeadline = screen.getByText(/Test Headline/i);
+  expect(testHeadline).toBeInTheDocument();
+})
